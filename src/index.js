568f12d7d52fa7ea2380a0fe9947caae234b90b4
Julian Swerdlin <julianswer36@gmail.com>
2022-01-04 20:13:43 +0100

    new files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import {Server as WebSocketServer} from 'socket.io';
+import http from 'http';
 
 const app = express();
+const server = http.createServer(app);
+const io = new WebSocketServer(server);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000)
+io.on('connection', (socket) => {
+    console.log('nueva conexion: ', socket.id);
+})
+
+server.listen(3000)
 console.log('Server is running on port', 3000);
