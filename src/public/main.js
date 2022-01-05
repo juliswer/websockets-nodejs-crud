a3b5f14f9f7e083db499effe67701c7c863ba3fa
Julian Swerdlin <julianswer36@gmail.com>
2022-01-05 18:13:41 +0100

    new

--- src/public/main.js
+++ src/public/main.js
@@ -1,2 +1,11 @@
 const socket = io();
 
+const noteForm = document.querySelector('#noteForm');
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+
+noteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    socket.emit('client:newnote');
+});
