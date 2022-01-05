b148994a60273fe99a6d2d7784524d9c4a67a1fa
Julian Swerdlin <julianswer36@gmail.com>
2022-01-05 18:38:51 +0100

    new

--- src/public/ui.js
+++ src/public/ui.js
@@ -1,5 +1,13 @@
 const notesList = document.querySelector('#notes');
 
 const appendNote = note => {
-    notesList.innerHTML += `<h1>${note.title}</h1>`;
+    console.log(note);
+    notesList.innerHTML += `
+    <div class="card card-body rounded-0">
+        <div>
+            <h1 class="h3 card-title">${note.title}</h1>
+        </div>
+        <p>${note.description}</p>
+    </div>
+    `;
 }
