089cdf5de870715c04d5f2381266bf54e50aa692
Julian Swerdlin <julianswer36@gmail.com>
2022-01-06 19:45:32 +0100

    enw

--- src/public/ui.js
+++ src/public/ui.js
@@ -3,7 +3,7 @@ const notesList = document.querySelector('#notes');
 const appendNote = note => {
     console.log(note);
     notesList.innerHTML += `
-    <div class="card card-body rounded-0">
+    <div class="card card-body rounded-0 mb-2">
         <div>
             <h1 class="h3 card-title">${note.title}</h1>
         </div>
